bce912fbfd34429f260021857b987a522a8bc610
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-16 14:56:36 +0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  );
 };
 
 export default App;
