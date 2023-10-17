a1b111b6bcc316939ce457dfbbebf3d2288b7dcd
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-17 23:36:48 +0700

    add pull refresh

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -1,28 +1,36 @@
 import { FC, useEffect } from "react";
 import { useAppDispatch, useAppSelection } from "../../redux/store";
 import { getAllUser } from "../../redux/slices/userSlice";
+import ReactPullToRefresh from "react-pull-to-refresh";
 
 const UserPage: FC = () => {
   const { data, loading } = useAppSelection((state) => state.users);
   const dispatch = useAppDispatch();
 
+  const fetchUsers = async () => {
+    await dispatch(getAllUser());
+  };
+
   useEffect(() => {
-    dispatch(getAllUser());
-  }, [dispatch]);
+    fetchUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (loading) {
     return <div>loading...</div>;
   }
 
   return (
-    <div>
-      <div>user</div>
+    <ReactPullToRefresh onRefresh={fetchUsers}>
       <div>
-        {data.map((item, index) => {
-          return <div key={index}>{item.email}</div>;
-        })}
+        <div>user</div>
+        <div>
+          {data.map((item, index) => {
+            return <div key={index}>{item.email}</div>;
+          })}
+        </div>
       </div>
-    </div>
+    </ReactPullToRefresh>
   );
 };
 
