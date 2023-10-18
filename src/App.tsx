ca7e7c7c4694af2d0db5bf4e23873c1b6ffc98d7
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-19 00:55:59 +0700

    use QueryProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import router from "./router";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </QueryClientProvider>
   );
 };
 
