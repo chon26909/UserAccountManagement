caef660c1bbe06a4fe6eda7545bf0b034570c306
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-20 01:08:09 +0700

    update user page

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -6,6 +6,8 @@ import UserList from "../../features/users/UserList";
 import "../../styles/user.scss";
 import InfiniteScroll from "react-infinite-scroll-component";
 import UserTable from "../../features/users/UserTable";
+import UserFilter from "../../features/users/UserFilter";
+import Button from "../../components/Button";
 
 const UserPage: FC = () => {
   const { data, loading, total } = useAppSelection((state) => state.users);
@@ -34,42 +36,56 @@ const UserPage: FC = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (loading) {
-    return <div>loading...</div>;
-  }
+  // if (loading) {
+  //   return <div>loading...</div>;
+  // }
 
   if (displayMode === "table") {
-    return <UserTable data={data} />;
+    return (
+      <>
+        <div className="flex justify-between items-end">
+          <h2 className="my-3 text-[32px] font-bold">
+            User Account Management
+          </h2>
+          <div>
+            <Button className="bg-primary w-[200px]">Add User</Button>
+          </div>
+        </div>
+
+        <UserFilter />
+        <UserTable loading={loading} data={data} />
+      </>
+    );
   }
 
   return (
-    // <ReactPullToRefresh onRefresh={fetchUsers}>
-    <InfiniteScroll
-      dataLength={data.length}
-      next={fetchMore}
-      hasMore={true}
-      loader={<h4>Loading...</h4>}
-      endMessage={
-        <p style={{ textAlign: "center" }}>
-          <b>Yay! You have seen it all</b>
-        </p>
-      }
-      refreshFunction={fetchUsers}
-      pullDownToRefresh
-      pullDownToRefreshThreshold={50}
-      pullDownToRefreshContent={
-        <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-      }
-      releaseToRefreshContent={
-        <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-      }
-    >
-      <div className="p-5">
-        <div>Total user {total} account</div>
-        <UserList users={data} />
-      </div>
-    </InfiniteScroll>
-    // </ReactPullToRefresh>
+    <>
+      <InfiniteScroll
+        dataLength={data.length}
+        next={fetchMore}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+        refreshFunction={fetchUsers}
+        pullDownToRefresh
+        pullDownToRefreshThreshold={50}
+        pullDownToRefreshContent={
+          <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
+        }
+        releaseToRefreshContent={
+          <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
+        }
+      >
+        <div className="p-5">
+          <div>Total user {total} account</div>
+          <UserList users={data} />
+        </div>
+      </InfiniteScroll>
+    </>
   );
 };
 
