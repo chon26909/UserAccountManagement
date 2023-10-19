56d12f3543b880fe2147e2d111147ef4953f466d
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-19 23:13:16 +0700

    user list

---
+++ src/features/users/UserCardLoading.tsx
@@ -0,0 +1,16 @@
+// import Skeleton from "react-loading-skeleton";
+// import "react-loading-skeleton/dist/skeleton.css";
+
+// import ReactPlaceholder from "react-placeholder";
+// import "react-placeholder/lib/reactPlaceholder.css";
+
+// const UserCardLoading = () => {
+//   return (
+//     <>
+//       <RectShape color="gray" style={{ width: 25, height: 70 }} />
+//       <TextBlock rows={6} color="blue" />
+//     </>
+//   );
+// };
+
+// export default UserCardLoading;
--- src/features/users/UserList.tsx
+++ src/features/users/UserList.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import { IUser } from "../../types/user";
 import UserCard from "./UserCard";
 import "../../styles/user.scss";
+// import UserCardLoading from "./UserCardLoading";
 
 interface IUserList {
   users: IUser[];
@@ -10,6 +11,7 @@ interface IUserList {
 const UserList: FC<IUserList> = ({ users }) => {
   return (
     <div className="userList">
+      {/* <UserCardLoading /> */}
       {users.map((user, index) => (
         <UserCard key={index} {...user} />
       ))}
