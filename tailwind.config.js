3b7688045170e35c76a4ffb4a197e1a8cde03c7c
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-20 01:05:32 +0700

    add global config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      prompt: ["Prompt", "Helvetica", "arial", "sans-serif"],
+      thonburi: ["Thonburi", "Helvetica", "arial", "sans-serif"],
+    },
+    colors: {
+      white: "var(--white)",
+      primary: "var(--primary-color)",
+      red: "var(--red-primary)",
+      green: "var(--green-primary)",
+      gray: "var(--gray-lighter)",
+      gray_text: "var(--gray-text)",
+      tranperent: "rgba(0,0,0,0)",
+      yellow: "var(--orange-primary)",
+      black: "rgb(0,0,0)",
+    },
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
   },
   plugins: [],
 };
