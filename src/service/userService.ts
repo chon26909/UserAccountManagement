c97bd02ababfd3bfc568d8e5503764ba081dcadd
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-21 23:42:40 +0700

    add create user

--- src/service/userService.ts
+++ src/service/userService.ts
@@ -1,28 +1,38 @@
-import axios from "axios";
-import ENDPOINTS from "../constant/endpoints";
-import { IUser } from "../types/user";
-import { IPagination } from "../types/pagination";
-import { IPost } from "../types/post";
+import axios from 'axios';
+import ENDPOINTS from '../constant/endpoints';
+import { IUser } from '../types/user';
+import { IPagination } from '../types/pagination';
+import { IPost } from '../types/post';
 
 export type IUserListRequest = {
-  limit: number;
-  skip: number;
-  q?: string;
+    limit: number;
+    skip: number;
+    q?: string;
 };
 
 export interface IUserListResponse extends IPagination {
-  users: IUser[];
+    users: IUser[];
 }
 
 export interface IUserPostListResponse extends IPagination {
-  posts: IPost[];
+    posts: IPost[];
+}
+
+export interface ICreateUesrRequest {
+    firstName: string;
+    lastName: string;
+    age: number;
 }
 
 export const getAllUser = (data: IUserListRequest) => {
-  return axios.get<IUserListResponse>(ENDPOINTS.USER_LIST, { params: data });
+    return axios.get<IUserListResponse>(ENDPOINTS.USER_LIST, { params: data });
 };
 
 export const getUserPostList = (userId: string) => {
-  const url = ENDPOINTS.USER_POST_LIST.replace(":userId", userId);
-  return axios.get<IUserPostListResponse>(url);
+    const url = ENDPOINTS.USER_POST_LIST.replace(':userId', userId);
+    return axios.get<IUserPostListResponse>(url);
+};
+
+export const createUser = (data: ICreateUesrRequest) => {
+    return axios.post(ENDPOINTS.CREATE_USER, data);
 };
