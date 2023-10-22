f70f51c063539a8dc77d61d00a551e5701678796
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 00:30:42 +0700

    header

--- src/features/users/HeaderPageUser.tsx
+++ src/features/users/HeaderPageUser.tsx
@@ -1,14 +1,23 @@
-import Button from "../../components/Button";
+import { useState } from 'react';
+import Button from '../../components/Button';
+import ModalCreateUser from './ModalCreateUser';
 
 const HeaderPageUser = () => {
-  return (
-    <div className="flex justify-between items-end">
-      <h2 className="my-3 text-[32px] font-bold">User Account Management</h2>
-      <div>
-        <Button className="bg-primary w-[200px]">Add User</Button>
-      </div>
-    </div>
-  );
+    const [show, setShow] = useState(true);
+
+    return (
+        <>
+            <div className='flex justify-between items-end'>
+                <h2 className='my-3 text-[32px] font-bold'>User Account Management</h2>
+                <div>
+                    <Button className='bg-primary w-[200px]' onClick={() => setShow(true)}>
+                        Add User
+                    </Button>
+                </div>
+            </div>
+            <ModalCreateUser show={show} close={() => setShow(false)} />
+        </>
+    );
 };
 
 export default HeaderPageUser;
