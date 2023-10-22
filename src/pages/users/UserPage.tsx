691d64b414bfecdbb12bc0e0883bdd8d737f0a68
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 00:31:15 +0700

    update user page

--- src/pages/users/UserPage.tsx
+++ src/pages/users/UserPage.tsx
@@ -1,83 +1,81 @@
-import { FC, useEffect, useState } from "react";
-import { useAppDispatch, useAppSelection } from "../../redux/store";
-import { getAllUser, getUserMore } from "../../redux/slices/userSlice";
-import UserList from "../../features/users/UserList";
-import InfiniteScroll from "react-infinite-scroll-component";
-import UserTable from "../../features/users/UserTable";
-import UserFilter from "../../features/users/UserFilter";
-import HeaderPageUser from "../../features/users/HeaderPageUser";
-import "../../styles/user.scss";
+import { FC, useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelection } from '../../redux/store';
+import { getAllUser, getUserMore } from '../../redux/slices/userSlice';
+import UserList from '../../features/users/UserList';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import UserTable from '../../features/users/UserTable';
+import UserFilter from '../../features/users/UserFilter';
+import HeaderPageUser from '../../features/users/HeaderPageUser';
+import '../../styles/user.scss';
+import DisplayMode from '../../features/users/DisplayMode';
 
 const UserPage: FC = () => {
-  const { data, loading, total } = useAppSelection((state) => state.users);
-  const dispatch = useAppDispatch();
+    const { data, loading, total } = useAppSelection((state) => state.users);
+    const dispatch = useAppDispatch();
 
-  const [page, setPage] = useState(1);
-  const [perPage] = useState(20);
+    const [page, setPage] = useState(1);
+    const [perPage] = useState(20);
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [displayMode, setDisplayMode] = useState<"grid" | "table">("table");
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [displayMode, setDisplayMode] = useState<'grid' | 'table'>('table');
 
-  const fetchUsers = async () => {
-    await dispatch(getAllUser({ limit: perPage, skip: (page - 1) * perPage }));
-  };
+    const fetchUsers = async () => {
+        await dispatch(getAllUser({ limit: perPage, skip: (page - 1) * perPage }));
+    };
 
-  const fetchMore = async () => {
-    const newPage = page + 1;
-    setPage(newPage);
-    await dispatch(
-      getUserMore({ limit: perPage, skip: (newPage - 1) * perPage })
-    );
-  };
+    const fetchMore = async () => {
+        const newPage = page + 1;
+        setPage(newPage);
+        await dispatch(getUserMore({ limit: perPage, skip: (newPage - 1) * perPage }));
+    };
 
-  useEffect(() => {
-    fetchUsers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    useEffect(() => {
+        fetchUsers();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-  // if (loading) {
-  //   return <div>loading...</div>;
-  // }
+    // if (loading) {
+    //   return <div>loading...</div>;
+    // }
 
-  if (displayMode === "table") {
+    if (displayMode === 'table') {
+        return (
+            <>
+                <DisplayMode mode={displayMode} setMode={setDisplayMode} />
+                <HeaderPageUser />
+                <UserFilter />
+                <UserTable loading={loading} data={data} />
+                {/* <ModalCreateUser show={false} close={() => {}} /> */}
+            </>
+        );
+    }
+
     return (
-      <>
-        <HeaderPageUser />
-        <UserFilter />
-        <UserTable loading={loading} data={data} />
-      </>
+        <>
+            <DisplayMode mode={displayMode} setMode={setDisplayMode} />
+            <InfiniteScroll
+                dataLength={data.length}
+                next={fetchMore}
+                hasMore={true}
+                loader={<h4>Loading...</h4>}
+                endMessage={
+                    <p style={{ textAlign: 'center' }}>
+                        <b>Yay! You have seen it all</b>
+                    </p>
+                }
+                refreshFunction={fetchUsers}
+                pullDownToRefresh
+                pullDownToRefreshThreshold={50}
+                pullDownToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>}
+                releaseToRefreshContent={<h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>}
+            >
+                <div className='p-5'>
+                    <div>Total user {total} account</div>
+                    <UserList users={data} />
+                </div>
+            </InfiniteScroll>
+        </>
     );
-  }
-
-  return (
-    <>
-      <InfiniteScroll
-        dataLength={data.length}
-        next={fetchMore}
-        hasMore={true}
-        loader={<h4>Loading...</h4>}
-        endMessage={
-          <p style={{ textAlign: "center" }}>
-            <b>Yay! You have seen it all</b>
-          </p>
-        }
-        refreshFunction={fetchUsers}
-        pullDownToRefresh
-        pullDownToRefreshThreshold={50}
-        pullDownToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8595; Pull down to refresh</h3>
-        }
-        releaseToRefreshContent={
-          <h3 style={{ textAlign: "center" }}>&#8593; Release to refresh</h3>
-        }
-      >
-        <div className="p-5">
-          <div>Total user {total} account</div>
-          <UserList users={data} />
-        </div>
-      </InfiniteScroll>
-    </>
-  );
 };
 
 export default UserPage;
