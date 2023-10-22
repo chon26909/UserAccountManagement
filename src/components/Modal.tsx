0fd251be8370bf8f93be54a1136b1c606af2de20
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 00:30:05 +0700

    change style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -32,7 +32,7 @@ const Modal: FC<IModalProps> = ({ children, isOpen, modalClassName, bodyClassNam
     return (
         <div
             className={`${
-                isOverlay ? 'custom-modal fixed inset-0 z-50 bg-black/50 flex items-center justify-center focus:outline outline-none' : ''
+                isOverlay ? 'custom-modal fixed inset-0 z-auto bg-black/50 flex items-center justify-center focus:outline outline-none' : ''
             } `}
         >
             <div
@@ -45,10 +45,26 @@ const Modal: FC<IModalProps> = ({ children, isOpen, modalClassName, bodyClassNam
                             overflow-hidden
                             relative rounded-md md:rounded-none
                             
-                            transition duration-200
-                            ${show ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}
+                            transition duration-1000
+                            ${show ? 'translate-y-0 opacity-100' : 'translate-y-96 opacity-0'}
                             `}
             >
+                {/* <div
+                className={[
+                    modalClassName,
+                    'transition duration-1000',
+                    show ? 'translate-y-0 opacity-100' : 'translate-y-96 opacity-0',
+                    show
+                        ? `  h-[600px] w-[1000px] 
+                                lg:h-[600px] lg:w-4/6
+                                md:h-full md:w-full 
+                            
+                                bg-white
+                                overflow-hidden
+                                relative rounded-md md:rounded-none`
+                        : ''
+                ].join(' ')}
+            > */}
                 <div className={[bodyClassName, 'h-full '].join(' ')}>{children}</div>
             </div>
         </div>
@@ -56,10 +72,17 @@ const Modal: FC<IModalProps> = ({ children, isOpen, modalClassName, bodyClassNam
 };
 
 export const ModalHeader: FC<{ children: JSX.Element; className?: string }> = ({ children, className }) => {
-    return <div className={[className, 'h-[70px] p-5 border-b-2 border-b-[#dedede] text-xl font-semibold'].join(' ')}>{children}</div>;
+    return <div className={[className, 'h-[55px] px-5 py-3 border-b-2 border-b-[#dedede] text-xl font-semibold'].join(' ')}>{children}</div>;
 };
+
+export const ModalBody: FC<{ children: JSX.Element | JSX.Element[]; className?: string }> = ({ children, className }) => {
+    return <div className={[className, 'p-5'].join(' ')}>{children}</div>;
+};
+
 export const ModalFooter: FC<{ children: JSX.Element; className?: string }> = ({ children, className }) => {
-    return <div className={[className, 'h-[80px] p-5'].join(' ')}>{children}</div>;
+    return (
+        <div className={[className, 'h-[70px] px-5 py-3 absolute bottom-0 right-0 w-full border-t-2 border-t-[#dedede]'].join(' ')}>{children}</div>
+    );
 };
 
 export default Modal;
