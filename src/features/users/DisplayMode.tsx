162f7c7b6b6b0976acd2074c11f79b05f401676d
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 23:50:13 +0700

    add icon

--- src/features/users/DisplayMode.tsx
+++ src/features/users/DisplayMode.tsx
@@ -1,18 +1,28 @@
-import { FC } from "react";
-import Button from "../../components/Button";
+import { FC } from 'react';
+import { BsTable, BsFillGrid3X2GapFill } from 'react-icons/bs';
 
 interface IDisplayMode {
-  mode: string;
-  setMode: (mode: "grid" | "table") => void;
+    mode: string;
+    setMode: (mode: 'grid' | 'table') => void;
 }
 
 const DisplayMode: FC<IDisplayMode> = (props) => {
-  return (
-    <div>
-      <Button onClick={() => props.setMode("table")}>table</Button>
-      <Button onClick={() => props.setMode("grid")}>grid</Button>
-    </div>
-  );
+    return (
+        <div className='m-3 flex justify-end text-2xl'>
+            <div
+                className={[props.mode === 'grid' ? 'bg-[#d7d7d7]' : '', 'p-2  rounded cursor-pointer'].join(' ')}
+                onClick={() => props.setMode('grid')}
+            >
+                <BsFillGrid3X2GapFill />
+            </div>
+            <div
+                className={[props.mode === 'table' ? 'bg-[#d7d7d7]' : '', 'p-2  rounded cursor-pointer'].join(' ')}
+                onClick={() => props.setMode('table')}
+            >
+                <BsTable />
+            </div>
+        </div>
+    );
 };
 
 export default DisplayMode;
