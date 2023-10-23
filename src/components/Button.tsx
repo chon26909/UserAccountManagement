c4fcbcc663ecfc4025931f5668fa35c302c4eec4
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 23:49:46 +0700

    change style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,23 @@
-import { ButtonHTMLAttributes, FC } from "react";
+import { ButtonHTMLAttributes, FC } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string;
-  full?: boolean;
+    className?: string;
+    full?: boolean;
 }
 
 const Button: FC<ButtonProps> = ({ children, className, full, ...other }) => {
-  return (
-    <button
-      {...other}
-      className={[
-        className,
-        full ? "w-full" : "min-w-[130px]",
-        "shadow-lg bg-primary py-[0.5rem] px-5 text-white rounded cursor-pointer select-none",
-      ].join(" ")}
-    >
-      {children}
-    </button>
-  );
+    return (
+        <button
+            {...other}
+            className={[
+                className,
+                full ? 'w-full' : 'min-w-[130px]',
+                'shadow-lg text-white bg-primary py-[0.5rem] px-5  rounded cursor-pointer select-none'
+            ].join(' ')}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
