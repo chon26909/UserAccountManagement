c7eaf90021fa3588f1afa24e0fd594f551e089a0
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-24 00:09:40 +0700

    style

--- src/features/users/UserCard.tsx
+++ src/features/users/UserCard.tsx
@@ -1,26 +1,28 @@
-import { FC } from "react";
-import { IUser } from "../../types/user";
-import "../../styles/user.scss";
-import { Link } from "react-router-dom";
+import { FC } from 'react';
+import { IUser } from '../../types/user';
+import '../../styles/user.scss';
+import { Link } from 'react-router-dom';
 
 interface IUserCard extends IUser {}
 
 const UserCard: FC<IUserCard> = (user) => {
-  return (
-    <div className="card">
-      <div>
-        <img src={user.image} alt={user.firstName} />
-      </div>
-      <div className="fullname">
-        Name : {user.firstName} {user.lastName}
-      </div>
-      <div>Gender : {user.gender}</div>
-      <div className="email"> Email : {user.email}</div>
-      <Link to={user.id + "/posts"}>
-        <div className="underline">see posts</div>
-      </Link>
-    </div>
-  );
+    const defaultImage = 'https://newadmin.heberjeunes.fr/images/no-photo.png';
+
+    return (
+        <div className='card'>
+            <div>
+                <img src={user.image || defaultImage} alt={user.firstName} />
+            </div>
+            <div className='fullname'>
+                Name : {user.firstName} {user.lastName}
+            </div>
+            <div>Gender : {user.gender}</div>
+            <div className='email'> Email : {user.email}</div>
+            <Link to={user.id + '/posts'}>
+                <div className='underline'>see posts</div>
+            </Link>
+        </div>
+    );
 };
 
 export default UserCard;
