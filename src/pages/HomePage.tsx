3de79d5f757020f12ec951412a213eb1d3be5da0
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-23 23:53:53 +0700

    update home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage: FC = () => {
-  return (
-    <div>
-      <div>Home Page</div>
-      <Link to="users">go to users page</Link>
-    </div>
-  );
+    return (
+        <div className='p-5'>
+            <div className='my-5'>
+                <Link to='users'>go to users page</Link>
+            </div>
+        </div>
+    );
 };
 
 export default HomePage;
