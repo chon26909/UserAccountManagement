618faee054b861b6befed105941af2e310eea4ff
HiChon <59905927+chon26909@users.noreply.github.com>
2023-10-24 00:09:48 +0700

    edit layout

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 const AdminLayout = () => {
-  return (
-    <div className="max-w-[1100px] mx-auto">
-      <Outlet />
-    </div>
-  );
+    return (
+        <div className='container mx-auto'>
+            <Outlet />
+        </div>
+    );
 };
 
 export default AdminLayout;
